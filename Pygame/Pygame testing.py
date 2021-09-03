6148967765ec58651338f32b0ff9da3e9dacbfa1
Azre <nicercool@gmail.com>
2021-09-03 21:38:12 +1200

    little edit

--- Pygame/Pygame testing.py
+++ Pygame/Pygame testing.py
@@ -1,6 +1,5 @@
 '''Animation time!!!!'''
 
-
 import pygame
 pygame.init()
 
@@ -12,10 +11,6 @@ color_rect = (100, 255, 100) # Color RGB tuple for rectangle drawing
 color_circ = (100, 100, 255) # Color RGB tuple for circle drawing
 color_arc = (100, 100, 100) # COlor RGB tuple for circle drawing
 
-def point (x, y):
-    return (x, y)
-x = 0
-y = 0
 
 # Fill screen display Surface
 screen.fill(color)
@@ -25,12 +20,7 @@ while loop == 1: # Pygame loop
     for event in pygame.event.get(): # Refresh event queue (THIS IS NECESSARY TO PREVENT FREEZING)
         if event.type == pygame.QUIT:
             loop = 0
-    if x or y != 700:
-        x += 2
-        y += 1
     
-        
-    pygame.draw.lines(screen, color_line, False, [point(x,y), point(x+1,y+1)], 4)
     
     pygame.display.update() # Call this for every edit to the screen (e.g. Animations)
     
