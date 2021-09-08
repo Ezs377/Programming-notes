88d1876e55fc42ac6ee417e362bac5a9a28f609b
Azre <nicercool@gmail.com>
2021-09-08 16:30:15 +1200

    Finished moving animation

--- Pygame/Pygame testing.py
+++ Pygame/Pygame testing.py
@@ -3,46 +3,14 @@
 import pygame, sys
 pygame.init()
 
-loop = 1
-screen = pygame.display.set_mode((600, 600))
-color = (0, 0, 0)
-rect_color = (255, 100, 100)
-clock = pygame.time.Clock()
-screen.fill(color)
-counter = 0
 
-pointlist = [(100, 100), (450, 100), (450, 450), (100, 450)]
-
-x = pointlist [counter][0]
-y = pointlist [counter][1]
-
-coordinates = (x, y)
-size = (50, 50)
-
-
-
+# While loop is still going i.e. program is still running
 while loop == 1:
-    for event in pygame.event.get(): # Refresh event queue (THIS IS NECESSARY TO PREVENT FREEZING)
-        if event.type == pygame.QUIT:
+    # Refresh event queue (THIS IS NECESSARY TO PREVENT FREEZING)
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: # If Pygame detects program shutdown
             loop = 0
     
-    # Grid lines
-    pygame.draw.lines(screen, (100, 255, 100), False, [(100, 0), (100, 600)], 1) # Left vertical
-    pygame.draw.lines(screen, (100, 255, 100), False, [(450, 0), (450, 600)], 1) # Right vertical
-    pygame.draw.lines(screen, (100, 255, 100), False, [(0, 100), (600, 100)], 1) # Upper horizontal
-    pygame.draw.lines(screen, (100, 255, 100), False, [(0, 450), (600, 450)], 1) # Lower horizontal
-    
-    clock.tick(10)
-    if x != pointlist[counter+1][0]:
-        x += 5
-    
-    
-    
-    coordinates = (x, y)
-    
-    screen.fill(color)
-    pygame.draw.rect(screen, rect_color, (coordinates, size), 0)
-    
-    pygame.display.update()
+    '''Do stuff here'''
 
-print ("done")
+print ("done") # Indicate finish
--- Pygame/Pygame.py
+++ Pygame/Pygame.py
@@ -57,59 +57,135 @@
     #screen.blit(ball, ballrect) # Redraw ball image by copy/pasting the ball image onto the display SUrface
     #pygame.display.flip() # Update everything
 #__________________________________________________________________________________________________________________________
-'''Animation'''
+#'''Animation'''
 
-import pygame
+#import pygame
+#pygame.init()
+
+#loop = 1 # For looping program
+#screen = pygame.display.set_mode((600,600)) # Set display Surface, with width and height tuple size
+#color = (5, 5, 5) # Color RGB tuple, represents a RGB color value
+#rect_color = (250, 100, 255)
+
+## Set a variable to the clock of Pygame
+#clock = pygame.time.Clock() 
+
+## Fill screen display Surface
+#screen.fill(color)
+
+## Set movement points
+#points = [(150,150, 50, 50), (450, 150, 50, 50), (450, 450, 50, 50), (150, 450, 50, 50)]
+
+#pointnum = 0
+
+#while loop == 1: # Pygame loop
+    
+    #'''Using the ingame clock'''
+    #clock.tick(10) # FPS
+    
+    #'''Using a time delay'''
+    ##pygame.time.delay(1000)   
+    
+    ##print (pygame.time.get_ticks()) # Total runtime in millisceconds
+    ##print (clock.get_time()) # Time in milliseconds from previous tick to the next
+    ##print (clock.get_fps()) # Current FPS
+    
+    #for event in pygame.event.get(): # Refresh event queue (THIS IS NECESSARY TO PREVENT FREEZING)
+        #if event.type == pygame.QUIT:
+            #loop = 0
+            
+    #screen.fill(color)
+    
+    #pygame.draw.rect(screen, rect_color, (points[pointnum]), 0)
+    
+    #pointnum += 1
+    #if pointnum == 4:
+        #pointnum = 0
+    
+    
+    #pygame.display.update() # Call this for every edit to the screen (e.g. Animations)
+    
+
+
+#print ("done")
+#__________________________________________________________________________________________________________________________
+'''Moving animation!'''
+import pygame, sys
 pygame.init()
 
-loop = 1 # For looping program
-screen = pygame.display.set_mode((600,600)) # Set display Surface, with width and height tuple size
-color = (5, 5, 5) # Color RGB tuple, represents a RGB color value
-rect_color = (250, 100, 255)
+loop = 1 # Instead of using sys.exit, use variable
+screen = pygame.display.set_mode((600, 600)) # Set display size
+color = (0, 0, 0) # Color RGB tuple
+rect_color = (255, 100, 100) # Color RGB tuple 2
+clock = pygame.time.Clock() # Create clock object
+screen.fill(color) # Fill display color
+counter = 0 # Counter value for coordinates
+increment = 10 # Value for setting the movement value of square
 
-# Set a variable to the clock of Pygame
-clock = pygame.time.Clock() 
+# List of coordinates to get square to move to
+pointlist = [(100, 100), (450, 100), (450, 450), (100, 450)]
 
-# Fill screen display Surface
-screen.fill(color)
+# Set x and y coordinates
+x = pointlist [counter][0]
+y = pointlist [counter][1]
 
-# Set movement points
-points = [(150,150, 50, 50), (450, 150, 50, 50), (450, 450, 50, 50), (150, 450, 50, 50)]
+# Set coordinates
+coordinates = (x, y)
 
-pointnum = 0
+# Size of square
+size = (50, 50)
 
-while loop == 1: # Pygame loop
-    
-    '''Using the ingame clock'''
-    clock.tick(10) # FPS
-    
-    '''Using a time delay'''
-    #pygame.time.delay(1000)   
-    
-    #print (pygame.time.get_ticks()) # Total runtime in millisceconds
-    #print (clock.get_time()) # Time in milliseconds from previous tick to the next
-    #print (clock.get_fps()) # Current FPS
-    
-    for event in pygame.event.get(): # Refresh event queue (THIS IS NECESSARY TO PREVENT FREEZING)
-        if event.type == pygame.QUIT:
+# While loop is still going i.e. program is still running
+while loop == 1:
+    # Refresh event queue (THIS IS NECESSARY TO PREVENT FREEZING)
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: # If Pygame detects program shutdown
             loop = 0
-            
+    
+    # Set FPS
+    clock.tick(30)
+    
+    # Refresh screen
     screen.fill(color)
     
-    pygame.draw.rect(screen, rect_color, (points[pointnum]), 0)
+    # Grid lines
+    pygame.draw.lines(screen, (100, 255, 100), False, [(100, 0), (100, 600)], 1) # Left vertical
+    pygame.draw.lines(screen, (100, 255, 100), False, [(500, 0), (500, 600)], 1) # Right vertical
+    pygame.draw.lines(screen, (100, 255, 100), False, [(0, 100), (600, 100)], 1) # Upper horizontal
+    pygame.draw.lines(screen, (100, 255, 100), False, [(0, 500), (600, 500)], 1) # Lower horizontal
     
-    pointnum += 1
-    if pointnum == 4:
-        pointnum = 0
     
+    # Code for coordinates seeking
+    if x < pointlist[counter+1][0]: # if x is less than the desired coordinate, add x
+        x += increment
+    elif x > pointlist[counter+1][0]: # if x is more than desired coordinate, subtract x
+        x -= increment
     
-    pygame.display.update() # Call this for every edit to the screen (e.g. Animations)
-    
+    # Same formula used for y
+    if y < pointlist[counter+1][1]:
+        y += increment
+    if y > pointlist[counter+1][1]:
+        y -= increment
 
 
-print ("done")
-#__________________________________________________________________________________________________________________________
+    # If the cooridnates match the target coordinates, move to the enxt coordinate
+    if coordinates == pointlist[counter+1]:
+        counter += 1
+    
 
+    if counter == 3:
+        counter = -1
+    
+    # Set coordinates of square
+    coordinates = (x, y)
+
+    # Draw square
+    pygame.draw.rect(screen, rect_color, (coordinates, size), 0)
+    
+    # Update screen
+    pygame.display.update()
+
+print ("done") # Indicate finish
 #__________________________________________________________________________________________________________________________
 
 #__________________________________________________________________________________________________________________________
